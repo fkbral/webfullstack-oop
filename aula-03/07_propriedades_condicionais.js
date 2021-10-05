a9bef0ab295d702ee8937b0ff3a983f5e99e9fa8
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2021-10-04 21:17:57 -0300

    feat: aula 03 - exercício do Filme

--- aula-03/07_propriedades_condicionais.js
+++ aula-03/07_propriedades_condicionais.js
@@ -1,2 +1,18 @@
-// class Filme terá propriedades ano, que não pode anteceder 1895 e categoria que
-// pode ser suspense, drama, ação, comédia e terror
+// class Filme terá propriedades ano, que não pode anteceder 1895 e categorias que
+// podem ser suspense, drama, ação, comédia e terror
+
+class Filme {
+  constructor(ano, categorias) {
+    const categoriasPermitidas = ["suspense", "drama", "ação", "comédia", "terror"]
+
+    const categoriasValidas = categorias.filter(categoria => {
+      return categoriasPermitidas.includes(categoria)
+    })
+
+    this.ano = (ano < 1895) ? 'N/A' : ano
+    this.categorias = categoriasValidas
+  }
+}
+
+const filme1 = new Filme(2010, ['ação', 'romance'])
+console.log(filme1)
